65642f10de3c004c74f8d056a57218985ffdc670
Ubuntu <ubuntu@ip-172-31-19-132.eu-west-1.compute.internal>
2025-09-18 07:24:50 +0000

    Dropped NaNs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -61,13 +61,13 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     df = modif_target_variable(df)
 
     # Replace values -1 and 0
-    #df = replace_values(df)
+    df = replace_values(df)
 
     # Drop columns
-    #df = drop_columns(df)
+    df = drop_columns(df)
 
     # Drop lines with NaN values
-    #df = drop_lines_with_nan_values(df)
+    df = drop_lines_with_nan_values(df)
 
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = split_data(df)
